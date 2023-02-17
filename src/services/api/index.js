4746b45bc791a9a723556ee5c91be2f97bae3716
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2023-02-17 17:24:04 -0300

    deploy

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: process.env.URL_BACK || 'http://localhost:4545/',
+  baseURL: process.env.URL_BACK,
   headers: {
     'Content-Type': 'application/json'
   },
