3ae7777b9871997cfdddfaa77733f154ca8b4902
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2023-02-17 17:19:30 -0300

    deploy

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: process.env.URL_BACK || 'http://localhost:4545/',
   headers: {
-    'Content-Type': 'application/json',
-    "ngrok-skip-browser-warning":"any"
+    'Content-Type': 'application/json'
   },
-  withCredentials: true
+  withCredentials: false,
+  mode: 'no-cors'
 })
