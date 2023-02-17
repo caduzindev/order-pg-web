1ca92f1adabe945b1b57e1ad0ff8022ae2762218
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2023-02-17 17:44:24 -0300

    deploy

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: process.env.URL_BACK,
+  baseURL: 'https://a9ea-2804-858-f23c-7600-802d-7e82-c7d3-8fbc.sa.ngrok.io',
   headers: {
     'Content-Type': 'application/json',
     "ngrok-skip-browser-warning": 'any'
